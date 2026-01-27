371f200a773b4d7e77e80469078b6f0e877ed983
Klaudia <120659110+KlaudiaKozakiewicz@users.noreply.github.com>
2026-01-28 00:35:13 +0100

    Add dotenv to load API key
    Load environment variables from .env file.

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import requests
 from streamlit_searchbox import st_searchbox
 from datetime import date
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 API_KEY = os.getenv("TMDB_API_KEY")
 
